cf198213b15aaafb15e0caac5af22fdbd60b6644
Long <dylant7192@gmail.com>
2024-05-13 17:55:38 +0100

    Added skeleton for movement and rotation

---
+++ Assets/Scripts/MainHandler.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Code that relates to the "bigger picture" should be called from here for clarity
+//So for example, baking navmeshes on Awake, or generating inventory on Start.
+public class MainHandler : MonoBehaviour
+{
+    
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public GameObject head;
+    public GameObject torso;
+    public GameObject legs;
+
+    private void FixedUpdate() {
+        
+    }
+
+    private void HandleMove() {
+        
+    }
+
+    private void HandleHeadRotation() {
+
+    }
+
+    private void HandleTorsoRotation() {
+
+    }
+}
